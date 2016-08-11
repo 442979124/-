49c644c2bb6864e808a6379a8b6869648cc74b1f
Popper Yang <apple@appledeiMac.local>
2016-08-11 15:25:58 +0800

    121

--- com.xjlx520.www/AppDelegate.m
+++ com.xjlx520.www/AppDelegate.m
@@ -21,7 +21,7 @@
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
     self.window.rootViewController = [[MainTableController alloc] init];
     [self.window makeKeyAndVisible];
-    
+    //qwqwqwqwqwesds
     return YES;
     
 }
